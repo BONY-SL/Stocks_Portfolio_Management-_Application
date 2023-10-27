ae513f805241e5ff242bc2a59f0d4ee1ed6533e0
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2023-10-28 00:14:30 +0530

    create database connection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.example.spms {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.logging;
+    requires java.sql;
 
     opens com.example.spms to javafx.fxml;
     exports com.example.spms;
