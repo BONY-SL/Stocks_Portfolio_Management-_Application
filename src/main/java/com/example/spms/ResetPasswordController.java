2e7d42e9454f33427fdf3f041139449def478364
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2023-10-29 23:25:52 +0530

    implemnt methods

--- src/main/java/com/example/spms/ResetPasswordController.java
+++ src/main/java/com/example/spms/ResetPasswordController.java
@@ -6,9 +6,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ResetPasswordController {
 
@@ -21,6 +28,13 @@ public class ResetPasswordController {
     @FXML
     Parent root;
 
+    @FXML
+    private TextField NIC;
+
+    @FXML
+    private Label userid;
+    private String userNIC=null;
+
     public void gotoLogin(ActionEvent event) throws IOException {
 
         Parent root= FXMLLoader.load(getClass().getResource("hello-view.fxml"));
@@ -30,4 +44,73 @@ public class ResetPasswordController {
         stage.setX(200);
         stage.setY(100);
     }
+
+    public void searchUserID(ActionEvent event)throws IOException{
+
+        userNIC=NIC.getText();
+        //System.out.println(userNIC);
+
+
+        if(userNIC.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Look, an Error Dialog");
+            alert.setContentText("Enter User NIC Number");
+            alert.showAndWait();
+
+        }else {
+            Connection conn= DatabaseConnection.getConnection();
+            int count1 = 0;
+            String Db_user = "select * from employee";
+
+            String coutemployee = "SELECT COUNT(*) FROM employee";
+            try {
+
+                Statement stmtcount = conn.createStatement();
+                ResultSet resaltcount = stmtcount.executeQuery(coutemployee);
+
+                while (resaltcount.next()) {
+                    count1 = resaltcount.getInt(1);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            try {
+                int count2 = 0;
+                Statement statment1 = conn.createStatement();
+                ResultSet resalt1 = statment1.executeQuery(Db_user);
+                while (resalt1.next()) {
+                    String nic = resalt1.getString(8);
+
+                    if (userNIC.equals(nic)) {
+                        String empid = resalt1.getString(1);
+                        userid.setText(empid);
+                        break;
+                    }
+                    ++count2;
+                }
+                if (count1 == count2) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Dialog");
+                    alert.setHeaderText("Look, an Error Dialog");
+                    alert.setContentText("Invalid NIC Number");
+                    alert.showAndWait();
+                    NIC.setText("");
+                }else{
+                    //System.out.println(userNIC);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }finally {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+    }
+
+
 }
--- src/main/java/com/example/spms/UserSessionSaved.java
+++ src/main/java/com/example/spms/UserSessionSaved.java
@@ -1,4 +1,45 @@
 package com.example.spms;
 
-public class UserSessionSaved {
+public final class UserSessionSaved {
+
+    private static String employee_id;
+    private static String password;
+    private static String employee_name;
+    private static String email;
+    private static String contact_number;
+    private static String dob;
+    private static String NIC;
+
+    private static UserSessionSaved instance;
+
+    UserSessionSaved(String empid, String pwd) {
+
+        this.employee_id=empid;
+        this.password=pwd;
+        System.out.println(employee_id);
+        System.out.println(password);
+
+    }
+
+    public static UserSessionSaved getInstance(String empid,String pwd) {
+        if(instance == null) {
+            instance = new UserSessionSaved( empid, pwd);
+        }
+        return instance;
+    }
+
+    public static String getEmployee_id(){
+        return employee_id;
+    }
+
+    public static String getEmployee_password(){
+        return password;
+    }
+
+    public static void cleanUserSession() {
+        employee_id = null;
+        password = null;
+        instance = null;
+    }
+
 }
