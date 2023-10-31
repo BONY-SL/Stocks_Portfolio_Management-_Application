55165cb309fe1e488d41f1bf401da55a8b1d04f2
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2023-10-31 15:57:41 +0530

    create profile UI

--- src/main/java/com/example/spms/MainDashboardController.java
+++ src/main/java/com/example/spms/MainDashboardController.java
@@ -6,10 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class MainDashboardController {
 
@@ -34,9 +37,22 @@ public class MainDashboardController {
 
     public void gotoLogin(ActionEvent event) throws IOException {
 
-        Parent root= FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Loginpage=new Scene(root);
-        stage.setScene(Loginpage);
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Look, a Confirmation Dialog");
+        alert.setContentText("Dou you want logout?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK
+            Parent root= FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+            Loginpage=new Scene(root);
+            stage.setScene(Loginpage);
+        } else {
+            // ... user chose CANCEL or closed the dialog
+        }
+
     }
 }
