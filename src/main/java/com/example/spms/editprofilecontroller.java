80f8b7718e617d85af37fe27cc2412cab36f9564
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2023-11-04 00:13:56 +0530

    Update editprofilecontroller.java

--- src/main/java/com/example/spms/editprofilecontroller.java
+++ src/main/java/com/example/spms/editprofilecontroller.java
@@ -7,7 +7,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
